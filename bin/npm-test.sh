cfaa454fe8a085e053a9010e1602ba84ed99d854
Ronen Babayoff <ronen@lavaina.com>
2014-12-24 02:58:39 -0400

    Use cake in npm-test.sh

--- bin/npm-test.sh
+++ bin/npm-test.sh
@@ -1,7 +1,6 @@
-#!/bin/bash
+#!/bin/bash -e
 
 coffee -o lib -c src
 coffee -o tests/lib -c tests/src
-ls tests/lib
-grep 'only' tests/lib/*
-mocha --colors --reporter spec tests/lib/*Test*.js
+#mocha --colors --reporter spec tests/lib/*Test*.js
+cake test-js
