b86693655865962c7d34e7cf3b9c6b652cf0aea7
Juan Sepulveda <juan@lavaina.com>
2016-04-21 16:40:13 -0400

    Fix travis CI test script

--- bin/npm-test.sh
+++ bin/npm-test.sh
@@ -1,5 +1,3 @@
 #!/bin/bash -e
 
-coffee -o lib -c src
-coffee -o tests/lib -c tests/src
-mocha --colors --reporter spec tests/lib/*Test*.js
+mocha --colors --compilers coffee:coffee-script/register --reporter spec tests/lib/*Test*.coffee
